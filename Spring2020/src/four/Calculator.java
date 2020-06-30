6d82357b24f15ec733f07cec21859bbe6b72ff16
EvyaBhal <evyatarbhal@gmail.com>
2020-06-30 23:15:43 +0300

    done question 1-4

--- Spring2020/src/four/Calculator.java
+++ Spring2020/src/four/Calculator.java
@@ -25,24 +25,21 @@ public class Calculator implements ActionListener {
     }
     private void initWindow() {
         JPanel pb = new JPanel();
-        pb.setLayout(new BoxLayout
-                (pb, BoxLayout.X_AXIS));
+        pb.setLayout(new BoxLayout(pb, BoxLayout.X_AXIS));
 
-        exit = new JButton("exit");
-        exit.addActionListener(this);
+       // exit = new JButton("exit");
+       // exit.addActionListener(this);
 
         add = new JButton("Add");
-        add.addActionListener(this);
+       // add.addActionListener(this);
         pb.add(add);
         clear = new JButton("Clear");
         clear.addActionListener (this);
         pb.add(clear);
 
         jp = new JPanel();
-        jp.setBorder(BorderFactory.
-                createLineBorder(Color.RED, 5));
-        jp.setLayout(new GridLayout
-                (0, 2, 10, 10));
+        jp.setBorder(BorderFactory.createLineBorder(Color.RED, 5));
+        jp.setLayout(new GridLayout(0, 2, 10, 10));
         JLabel l1 = new JLabel("First Number");
         l1.setSize(50,10);
         jp.add(l1);
@@ -59,18 +56,17 @@ public class Calculator implements ActionListener {
         lr.setSize(50,10);
         jp.add(lr);
         result = new JTextField(20);
-        result.setEditable(false);
+       // result.setEditable(false);
         jp.add(result);
         jp.add(pb);
 
         jf = new JFrame();
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jf.setLayout(new BoxLayout(
-                jf.getContentPane(), BoxLayout.Y_AXIS));
+        jf.setLayout(new BoxLayout(jf.getContentPane(), BoxLayout.Y_AXIS));
         jf.setPreferredSize(new Dimension(400,200));
         jf.setTitle("Adder");
         jf.add(jp);
-        jf.add(exit);
+        //jf.add(exit);
         jf.pack();
         jf.setVisible(true);
     }
@@ -78,20 +74,20 @@ public class Calculator implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
         if (actionEvent.getSource() == add){
-            try {
+           // try {
                 double d1 = Double.parseDouble(first.getText());
                 double d2 = Double.parseDouble(second.getText());
                 double r = d1 + d2;
                 result.setText(Double.toString(r));
-            } catch (Exception e) {
-                result.setText("invalid values");
-            }
+            //} catch (Exception e) {
+              //  result.setText("invalid values");
+            //}
         } else if (actionEvent.getSource() == exit){
             System.exit(1);
-        } else {
+        }/* else {
             first.setText("");
             second.setText("");
             result.setText("");
-        }
+        }*/
     }
 }
